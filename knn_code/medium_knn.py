5babf1e73d3ea448c30f696105cbe0917b069265
Thomas Boyle <90807000+ThomasStatic@users.noreply.github.com>
2022-03-28 04:58:11 -0400

    KNN Code

--- knn_code/medium_knn.py
+++ knn_code/medium_knn.py
@@ -16,6 +16,7 @@ class KNN:
 	def predict(self, X_test, Y_test):
 		predictions = []
 
+
 		for i in range(len(X_test)):
 			dist = np.array([euc_dist(X_test[i], x_t) for x_t in self.X_train])
 			dist_sorted = dist.argsort()[:self.K]
@@ -27,8 +28,8 @@ class KNN:
 					neigh_count[self.Y_train[idx]] = 1
 			sorted_neigh_count = sorted(neigh_count.items(), key = operator.itemgetter(1), reverse = True)
 			predictions.append(sorted_neigh_count[0][0])
-			print(f"I think this number is... {sorted_neigh_count[0][0]}")
-			print(f"This number is actually: {Y_test[i]}")
+			#print(f"I think this number is... {sorted_neigh_count[0][0]}")
+			#print(f"This number is actually: {Y_test[i]}")
 		return predictions
 
 
@@ -55,4 +56,3 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.25, random
 model = KNN(K = 3)
 model.fit(X_train, y_train)
 pred = model.predict(X_test, y_test)
-#print(pred)
