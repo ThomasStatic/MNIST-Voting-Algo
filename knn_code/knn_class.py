e03626d8611cae1120ae2f304f858ba611703083
Thomas Boyle <90807000+ThomasStatic@users.noreply.github.com>
2022-03-29 16:08:04 -0400

    Update knn_class.py
    -Added in cross validation
    
    -Added in algorithm to find optimal k value

--- knn_code/knn_class.py
+++ knn_code/knn_class.py
@@ -2,13 +2,83 @@ import numpy
 import operator
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split, cross_val_score
+import pandas
+import matplotlib.pyplot as mplplt
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.patches as mpatches
+from sklearn import svm, datasets
+
+#Import the MNIST data set
+mnist_dataset = load_digits()
+
+X = mnist_dataset.data
+y = mnist_dataset.target
+
+# NOTE: You can randomize this split with a seed using random_state = 1-999
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+
+# Empty list to store the results of our cross validation
+cross_validation_scores = []
+
+# Create a list of the potential (odd) values of k (k as in KNN, 10 folds used)
+potential_k_values = list(range(1, int(len(X) * ((9)/10))))
+# Strip all multiples of 3 from the list
+potential_k_values = [k for k in potential_k_values if k % 3 != 0]
+
+# Perform cross validation:
+
+test_model = KNeighborsClassifier(n_neighbors = 1)
+test_model.fit(X,y)
+
+# A list to store the values of k we want to later plot
+k_values_for_graph = []
+
+for k in potential_k_values:
+	test_model = KNeighborsClassifier(n_neighbors = k)
+
+	# 10 folds used for cross validation
+	scores = cross_val_score(test_model, X, y, cv = 10, scoring = 'accuracy')
+	
+	# Take the average accuracy score from each test
+	mean = scores.mean()
+
+	# Add the mean score to the list for future use
+	cross_validation_scores.append(mean)
+	#print(f"(KNN) k value: {k}  Accuracy: {mean}")
+
+	k_values_for_graph.append(k)
+
+	# We must break out of the loop as the length of the potential k values is extremely long
+	# with results that are easily predictable after having noticed the general data trend
+	if k > 150:
+		break
+
+# List to store the value of error at all different k's
+errors = [1-x for x in cross_validation_scores]
+
+# The optimal k is the k that yields the highest accuracy
+optimal_k = potential_k_values[cross_validation_scores.index(max(cross_validation_scores))]
+
+print(f"The optimal k value is {optimal_k}")
+
+
+# Plot the data found
+mplplt.plot(k_values_for_graph, cross_validation_scores)
+mplplt.xlabel("Number of Neighbours K")
+mplplt.ylabel("Accuracy")
+mplplt.title("Number of Neighbours vs Accuracy of Prediction", size = 16)
+mplplt.show()
+
+
+
+
 
 
 class KNN:
 	"""A class to manage the functionalities of the KNN model"""
 
 
-	def __init__(self, x_train, y_train, k = 3):
+	def __init__(self, x_train, y_train, k = optimal_k):
 		"""Initialize all the basic attributes of the model"""
 
 		# Import note: To distinguish between the number of folds and the k 
@@ -18,6 +88,7 @@ class KNN:
 		# Fit the data for the model
 		self.X_train = x_train
 		self.Y_train = y_train
+
 
 		# Default the print predictions flag to false
 		self.print_predictions = 0
@@ -95,19 +166,5 @@ class KNN:
 			self.print_predictions = 0
 			print("Prediction results will no longer be printed!")
 			return 0
-
-
-#Import the MNIST data set
-mnist_dataset = load_digits()
-
-X = mnist_dataset.data
-y = mnist_dataset.target
-
-# NOTE: You can randomize this split with a seed using random_state = INT
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
-
-setup_model = KNN(X_train, y_train, k = 3)
-
-
 
 
